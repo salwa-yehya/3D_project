c3febecce60a3f268cfad15c992924c053428748
unknown <salwayehyaabuafash@gmail.com>
2023-04-08 16:05:04 +0300

    fixMouseMove

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import './style.css'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import gsap from 'gsap';
 
-//scene
+////scene
 const scene = new THREE.Scene();
 
 //sphere 
@@ -11,33 +11,34 @@ const geometry = new THREE.SphereGeometry( 3, 64, 64 ); //(radius , widthsegment
 const material = new THREE.MeshStandardMaterial( { 
     color: '#00ff83' ,
     roughness :0.7 ,
+    // wireframe :true ,
 
 } );
 const mesh = new THREE.Mesh( geometry, material );
 scene.add( mesh );
 
-//size 
+////size 
 const size ={
     width : window.innerWidth ,
     height : window.innerHeight,
 }
 
 
-//light 
+////light 
 const light = new THREE.PointLight( 0xffffff, 1, 100 );
 light.position.set( 0, 10, 10 ); //(x , y ,z )postion
 light.intensity =1.25   //defult=1
 scene.add( light );
 
 
-//Camera
+////Camera
 const camera = new THREE.PerspectiveCamera( 45, size.width/size.height , 0.1 , 100);
 camera.position.z = 20
 scene.add( camera );
 
 
 
-//renderer
+////renderer
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({canvas}); 
 renderer.setSize( size.width ,size.height)
@@ -45,7 +46,7 @@ renderer.setPixelRatio(2) //make surface more smoother
 renderer.render( scene ,camera )
 
 
-//controls 
+////controls 
 const controls = new OrbitControls( camera, canvas );
 controls.enableDamping = true  //used to give a sense of weight to the controls
 controls.enablePan = false    //disable camera panning.
@@ -55,7 +56,7 @@ controls.autoRotateSpeed  = 6  //should autoRotate  = true  && update controls
  
 
 
-//Resize in any width
+////Resize in any width
 window.addEventListener( 'resize' , () =>{
     //Update sizes
     size.width = window.innerWidth 
@@ -75,7 +76,7 @@ const loop =() =>{
 }
 loop()
 
-//Timeline    
+////Timeline    
  //->//gsap dont work
 const TI = gsap.timeline({defaults :{duration :1} })
 TI.fromTo(mesh.scale , {z:0 , x:0 , y:0} , {z:1 , x:1 , y:1})
@@ -83,21 +84,22 @@ TI.fromTo("nav" , {y :"-100%" } , {y :"0%" })
 TI.fromTo(".title", {opacity :0 } , {opacity:1})
 
 
-//mouse animation
+////mouse animation
 let mouseDown = false;
 let rgb = [];
 window.addEventListener("mousedown" , () => (mouseDown = true));
 window.addEventListener("mouseup" , () => (mouseDown = false));
 window.addEventListener("mousemove" , (e) => {
+    if(mouseDown){
     rgb = [
       Math.round((e.pageX / size.width) * 255),
       Math.round((e.pageY / size.height) * 255),
       150, // or any value you want for the blue channel
     ];
+}
     console.log(rgb);
     //animate
     let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
-    // new THREE.Color(`rgb(0,100,150)`)
     gsap.to(mesh.material.color , { r:newColor.r , g:newColor.g , b:newColor.b })
      
 });
